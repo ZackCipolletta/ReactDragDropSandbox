1c3cffa3ff4f11276af789833335e174a3004395
Zachary <ZackCipolletta@gmail.com>
2023-06-16 12:03:24 -0700

    attempting to isolate right half of screen.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import { Draggable } from './Draggable';
 import './App.css';
 
-export const Single = () => (
-  <Draggable
-    onDragStart={(rect) => { console.log('onDragStart => ', rect) }}
-    onDragEnd={(rect) => { console.log('onDragEnd => ', rect) }}
-  >
-    <div>Draggable</div>
-  </Draggable>
-);
+// if/then width/height (if w>h: divide l/r, if h>w: divide top/bottom)
 
-export const Multiple = () => (
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const adjustedW = w * .8;
+const adjustedH = h * .8;
+
+let left;
+let right;
+
+if (w > h) {
+  left = adjustedW * 0.5
+} else if (h > w) {
+  const top = adjustedH * 0.5
+  const bottom = h - adjustedH
+}
+
+const Multiple = () => (
   <main>
-    <Draggable onDragStart={(rect) => { console.log('onDragStart => ', rect) }}>
+    <Draggable onDragStart={(rect) => { console.log('onDragStart => ', rect) }} style={{ left: Math.floor(Math.random() * left), top: Math.floor(Math.random() * adjustedH) }} >
       <div>Draggable 1</div>
-    </Draggable>
-
-    <Draggable
-      style={{ left: '200px', top: '200px' }}
-    >
-      <div style={{ backgroundColor: '#2196f3' }}>Draggable 2</div>
+      
     </Draggable>
   </main>
 );
+
+export default Multiple;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Single, Multiple } from './App';
+import Multiple from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Single />
     <Multiple />
   </React.StrictMode>,
   document.getElementById('root')
