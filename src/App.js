5b4e585924b3ff01c978ed5d9f21c38ce4ba75cb
Zachary <ZackCipolletta@gmail.com>
2023-06-16 15:12:20 -0700

    Split screen into two halves, questions display on left, answers on right.

--- src/App.js
+++ src/App.js
@@ -16,18 +16,75 @@ let right;
 
 if (w > h) {
   left = adjustedW * 0.5
+  right = w - adjustedW * .5
+  // wide = [left, right];
+  // return wide;
+  //place className 'question' in left
 } else if (h > w) {
   const top = adjustedH * 0.5
   const bottom = h - adjustedH
 }
 
+// make object appear anywhere in the page
+// style={{ left: Math.floor(Math.random() * adjustedW), top: Math.floor(Math.random() * adjustedH) }}
+
+
+
 const Multiple = () => (
   <main>
-    <Draggable onDragStart={(rect) => { console.log('onDragStart => ', rect) }} style={{ left: Math.floor(Math.random() * left), top: Math.floor(Math.random() * adjustedH) }} >
-      <div>Draggable 1</div>
+
+    <Draggable onDragStart={(rect) => { console.log('onDragStart => ', rect) }} 
+    
+      style={{
+        left: Math.floor(Math.random() * (left * .8)),
+        top: Math.floor(Math.random() * adjustedH)
+      }} 
+    >
+      <div>question 1</div>
       
     </Draggable>
+
+    <Draggable
+      style={{
+        left: Math.floor(Math.random() * (left * .8)),
+        top: Math.floor(Math.random() * adjustedH)
+      }}
+    >
+      <div style={{ backgroundColor: '#2196f3' }}>question 2</div>
+    </Draggable>
+
+    <Draggable
+      style={{
+        left: Math.floor(Math.random() * (left * .8)),
+        top: Math.floor(Math.random() * adjustedH)
+      }}
+    >
+      <div style={{ backgroundColor: '#1fb230' }}>question 3</div>
+    </Draggable>
+
+
+    <Draggable
+      style={{
+        left: left + Math.floor(Math.random() * (right * .8)),
+        top: Math.floor(Math.random() * adjustedH)
+      }}
+    >
+      <div style={{ backgroundColor: '#7a1fb2' }}>Answer 1</div>
+    </Draggable>
+
+    <Draggable 
+      style={{
+        left:
+          left + Math.floor(Math.random() * (right * .8)), 
+          top: Math.floor(Math.random() * adjustedH)
+      }} 
+    >
+    <div style={{ backgroundColor: '#e928a9'}}>Answer 2</div>
+    </Draggable>  
+
   </main>
 );
 
 export default Multiple;
+
+
